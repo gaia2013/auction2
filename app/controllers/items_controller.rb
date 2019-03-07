d52467c3299f678ad0c9e35038b5076c3b0270ff
Hitoshi <hitoshi.gaia.galaxy@gmail.com>
2019-03-07 19:37:00 +0900

    create function of edit update destroy

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -15,14 +15,37 @@ class ItemsController < ApplicationController
     @item = Item.new(item_params)
 
     # 上の行はインスタンスを作っただけ。saveメソッドで保存しないと、消える。
-    @item.save
+    if @item.save
 
     # saveの後にshow(商品詳細ページ)に飛ぶ設定をします。
     # これを設定しないと、プログラムが行き場をなくしてエラーが起きます。
-    redirect_to @item
+      # redirect_to @item
+      # redirect_to item_url(@item.id)
+      # リンク先のパスとしてモデルオブジェクトが渡された場合、
+      # Railsはオブジェクトを一意に表す値、つまり、idを取得しようします。だから最終的には、redirect_to @itemは、redirect_to item_url(@item.id)と等価となります
 
-    # redirect_to "/items"
-    # だとindexページにリダイレクトする
+      redirect_to "/items"
+      # だとindexページにリダイレクトする
+    else
+      render 'new'
+
+    end
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    redirect_to @item
+  end
+
+  def destroy
+    @item = Item.find(params[:id])
+    @item.destroy
+    redirect_to items_path
   end
 
   private
@@ -31,3 +54,4 @@ class ItemsController < ApplicationController
       params.require(:item).permit(:name, :price, :seller, :description, :email, :image_url)
     end
 end
+# <input type="datetime" value="2019-03-07 10:20:47.681420"" name="item[created_at]" id="item_created_at">
